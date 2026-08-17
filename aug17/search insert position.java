9ea923c49cdb2bff2cb909587c5f10dd521e3de9
sreeja-dende <154778149+sreeja-dende@users.noreply.github.com>
2026-08-17 15:01:08 +0530

    Create search insert position.java

---
+++ aug17/search insert position.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+      int l=0;
+      int r=nums.length;
+      while(l<r)
+      { int mid=(r+l)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+
+        }
+        else if(nums[mid]>target)
+        {
+            r=mid;
+        }
+        else
+        {
+            l=mid+1;
+        }
+      }
+    return l;
+    }
+}
